34cf1fe02b36093c08d4c823303ac4a0f6a15c1b
Bharathraj <108296861+Bharathsbr@users.noreply.github.com>
2024-06-20 01:03:28 +0530

    Update BusJdbc.java

--- BusJdbc.java
+++ BusJdbc.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class BusJdbc {
     private static final String url="jdbc:mysql://localhost:3306/BusReservation";
     private static final String username="root";
-    private static final String password="bharath@S21";
+    private static final String password="*******";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url,username,password);
